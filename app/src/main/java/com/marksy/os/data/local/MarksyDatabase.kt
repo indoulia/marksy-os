6db31dcd7ced9be70472cbd51084e3474f2c258e
INDOULIA <indoulia@users.noreply.github.com>
2026-09-12 09:26:11 +0530

    feat: bump database for Marksy response persistence

--- app/src/main/java/com/marksy/os/data/local/MarksyDatabase.kt
+++ app/src/main/java/com/marksy/os/data/local/MarksyDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [NotificationEventEntity::class],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class MarksyDatabase : RoomDatabase() {
@@ -23,7 +23,7 @@ abstract class MarksyDatabase : RoomDatabase() {
                     MarksyDatabase::class.java,
                     "marksy_os.db"
                 )
-                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4)
+                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5)
                     .build()
                     .also { INSTANCE = it }
             }
